e1eeff818bcd9b52cabed16ad446cd897bdcb23c
Achilles <demateraachillescraignoelj@gmail.com>
2026-08-28 02:24:16 +0800

    asdasd

--- components/LearningsComplete.js
+++ components/LearningsComplete.js
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 import { useProgress } from './ProgressContext';
 
 export default function LearningsComplete() {
-  const { doneCount, modules, markLearningsComplete } = useProgress();
+  const { modules, markLearningsComplete } = useProgress();
   
   const learningModules = ['lab', 'detective', 'clickbait', 'images', 'scenarios'];
   const completedCount = learningModules.filter(k => modules[k]).length;
