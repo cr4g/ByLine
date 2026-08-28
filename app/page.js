ce2366fdcd96a4d1d59625bf845bfd0099a76555
Achilles <demateraachillescraignoelj@gmail.com>
2026-08-28 23:35:32 +0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,6 +6,11 @@ import Nav from '../components/Nav';
 import Hero from '../components/Hero';
 import PreTest from '../components/PreTest';
 import LearningPages from '../components/LearningPages';
+import SourceLab from '../components/SourceLab';
+import FakeNewsDetective from '../components/FakeNewsDetective';
+import ClickbaitChallenge from '../components/ClickbaitChallenge';
+import ImageInvestigation from '../components/ImageInvestigation';
+import ScenarioSimulator from '../components/ScenarioSimulator';
 import Quiz from '../components/Quiz';
 import FinalChallenge from '../components/FinalChallenge';
 import Footer from '../components/Footer';
@@ -26,8 +31,13 @@ export default function Home() {
         <LearningPages />
       </SequentialGate>
       
-      {/* Phase 3: Post-Test */}
+      {/* Phase 3: Post-Test (includes all activities) */}
       <SequentialGate phase="posttest">
+        <SourceLab />
+        <FakeNewsDetective />
+        <ClickbaitChallenge />
+        <ImageInvestigation />
+        <ScenarioSimulator />
         <Quiz />
         <FinalChallenge />
       </SequentialGate>
